c63799e09d333f58846f963620f2d87fcb441f18
Khant Wai Yan <52145576+WaiYan70@users.noreply.github.com>
2024-07-27 22:29:56 +0630

    adjusting the Floating Navbar and still fixing the hydration error from RecentProjects Section

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -7,7 +7,7 @@ import { FaLocationArrow } from 'react-icons/fa'
 
 const RecentProjects = () => {
   return (
-    <div className='py-20'>
+    <div className='py-20' id='projects'>
         <h2 className='heading'>
             A small section of {''}
             <span className='text-purple'>Recent Projects</span>
